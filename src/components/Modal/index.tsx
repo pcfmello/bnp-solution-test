df5550ff75a4c3782a69a68d91f20854053052f5
Paulo Cesar Ferreira de Mello <paulomello@gew.com.br>
2024-02-03 16:27:29 -0300

    fix(modal): Fixing hide modal when to click inside it except in buttons

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,16 +1,17 @@
-import styles from './style.module.css';
+import { useRef } from "react";
+import styles from "./style.module.css";
 
 type ModalProps = {
-	children: React.ReactNode;
-	title: string;
-	isOpen: boolean;
-	onClose?: (type: 'click' | 'esc', target: EventTarget) => void;
-	onConfirm?: () => void;
-	footer?: {
-		hidden?: boolean;
-		confirmText?: string;
-		cancelText?: string;
-	};
+  children: React.ReactNode;
+  title: string;
+  isOpen: boolean;
+  onClose?: (type: "click" | "esc", target: EventTarget) => void;
+  onConfirm?: () => void;
+  footer?: {
+    hidden?: boolean;
+    confirmText?: string;
+    cancelText?: string;
+  };
 };
 
 /* 
@@ -19,46 +20,82 @@ type ModalProps = {
 	- Ao clicar no wrapper do modal, o modal deve ser fechado, porém esta ação deve ser ignorada caso o usuário clique em qualquer elemento dentro do modal
 */
 
-export const Modal: React.FC<ModalProps> = ({ children, title, isOpen, ...props }) => {
-	function handleCloseClick(e: React.MouseEvent) {
-		props.onClose?.('click', e.target);
-	}
+export const Modal: React.FC<ModalProps> = ({
+  children,
+  title,
+  isOpen,
+  ...props
+}) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const closeButtonRef = useRef<HTMLButtonElement>(null);
+  const cancelButtonRef = useRef<HTMLButtonElement>(null);
 
-	function handleConfirmClick(e: React.MouseEvent) {
-		props.onConfirm?.();
-	}
+  function isCloseClickInvalid(e: React.MouseEvent): boolean {
+    const target = e.target as HTMLElement;
+    return (
+      !!containerRef.current?.contains(target) &&
+      !closeButtonRef.current?.contains(target) &&
+      !cancelButtonRef.current?.contains(target)
+    );
+  }
 
-	function handleKeyDown(e: React.KeyboardEvent<HTMLDivElement>) {
-		if (e.key === 'Escape') props.onClose?.('esc', e.target);
-	}
+  function handleCloseClick(e: React.MouseEvent) {
+    if (isCloseClickInvalid(e)) return;
+    props.onClose?.("click", e.target);
+  }
 
-	if (!isOpen) return null;
+  function handleConfirmClick(e: React.MouseEvent) {
+    props.onConfirm?.();
+  }
 
-	return (
-		<div data-modal-wrapper className={styles.wrapper} onClick={handleCloseClick} onKeyDown={handleKeyDown}>
-			<div data-modal-container>
-				<header data-modal-header>
-					<h2>{title}</h2>
+  function handleKeyDown(e: React.KeyboardEvent<HTMLDivElement>) {
+    if (e.key === "Escape") props.onClose?.("esc", e.target);
+  }
 
-					<button data-modal-close onClick={handleCloseClick}>
-						X
-					</button>
-				</header>
+  if (!isOpen) return null;
 
-				{children}
+  return (
+    <div
+      data-modal-wrapper
+      className={styles.wrapper}
+      onClick={handleCloseClick}
+      onKeyDown={handleKeyDown}
+    >
+      <div data-modal-container ref={containerRef}>
+        <header data-modal-header>
+          <h2>{title}</h2>
 
-				{!props.footer?.hidden && (
-					<div data-modal-footer>
-						<button data-modal-cancel onClick={handleCloseClick}>
-							{props.footer?.cancelText ?? 'Cancelar'}
-						</button>
+          <button
+            data-modal-close
+            ref={closeButtonRef}
+            onClick={handleCloseClick}
+          >
+            X
+          </button>
+        </header>
 
-						<button data-modal-confirm onClick={handleConfirmClick} data-type="confirm">
-							{props.footer?.confirmText ?? 'Confirmar'}
-						</button>
-					</div>
-				)}
-			</div>
-		</div>
-	);
+        {children}
+
+        {!props.footer?.hidden && (
+          <div data-modal-footer>
+            <button
+              data-modal-cancel
+              ref={cancelButtonRef}
+              onClick={handleCloseClick}
+            >
+              {props.footer?.cancelText ?? "Cancelar"}
+            </button>
+
+            <button
+              data-modal-confirm
+              onClick={handleConfirmClick}
+              data-type="confirm"
+            >
+              {props.footer?.confirmText ?? "Confirmar"}
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
