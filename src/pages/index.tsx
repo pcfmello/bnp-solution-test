94ea8712738591cab46e51ba26e6dee4d8caf1e8
Paulo Cesar Ferreira de Mello <paulomello@gew.com.br>
2024-02-04 17:43:51 -0300

    refactor(index): Creating links array containing link address and it label and then added it in the repeat function.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,30 @@
 import styles from "@/styles/home.module.css";
 import Link from "next/link";
 
+interface ILink {
+	linkTo: string;
+	label: string;
+}
+
 export default function Home() {
+	const links: ILink[] = [
+		{ linkTo: 'modal', label: 'Modal'},
+		{ linkTo: 'modal-de-confirmacao', label: 'Modal de confirmação'},
+		{ linkTo: 'lista', label: 'Lista'},
+		{ linkTo: 'formulario', label: 'Formulário'},
+		{ linkTo: 'context-api', label: 'Context API'},
+		{ linkTo: 'pagina-estatica', label: 'Pagina estática'},
+		{ linkTo: 'ciclo-de-vida', label: 'Ciclo de vida de componente'},
+	];
+
   return (
     <div className={styles.container}>
       <ul>
-        <li>
-          <Link href="/modal">Modal</Link>
-        </li>
-
-        <li>
-          <Link href="/modal-de-confirmacao">Modal de confirmação</Link>
-        </li>
-
-        <li>
-          <Link href="/lista">Lista</Link>
-        </li>
-
-        <li>
-          <Link href="/formulario">Formulário</Link>
-        </li>
-
-        <li>
-          <Link href="/context-api">Context API</Link>
-        </li>
-
-        <li>
-          <Link href="/pagina-estatica">Pagina estática</Link>
-        </li>
-
-        <li>
-          <Link href="/ciclo-de-vida">Ciclo de vida de componente</Link>
-        </li>
+        {links.map(link => (
+					<li>
+          	<Link href={link.linkTo}>{link.label}</Link>
+        	</li>
+				))}
       </ul>
     </div>
   );
